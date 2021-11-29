fc452f5447f58c22b78e3bae3edeb6b0059919e4
imolina212 <86175612+imolina212@users.noreply.github.com>
2021-11-29 18:04:33 -0500

    passing all tests

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,30 @@
+import React from "react";
 import PetList from "./PetList";
 import "./Employee.css";
 
-export const Employee = () => {
-  return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
-  );
-};
+class Employee extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      clicked: false,
+    }
+  }
+
+  render() {
+
+    return (
+      <article className="employee">
+        <h3>Staff Member Name: {this.props.eachPerson.prefix} {this.props.eachPerson.firstName} {this.props.eachPerson.lastName}, {this.props.eachPerson.postfix}</h3>
+        <h4>Staff Member Title</h4>
+        <button onClick={() => this.setState({ clicked: !this.state.clicked })}>Show Pets</button>
+        {this.state.clicked ? <PetList id={this.props.eachPerson.id} /> : null}
+
+
+        {/* <button onClick={() => this.setState({ clicked: !this.state.clicked})}>Guesstimate</button>
+            {this.state.clicked ? <Cart name={this.state.name}/> : null} */}
+      </article>
+    );
+  };
+}
 
 export default Employee;
