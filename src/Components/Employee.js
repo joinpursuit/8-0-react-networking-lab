73da00aa8f3cf0d96ff6e432382b9f93dca793fd
Nima sherpa <Toplouie78@gmail.com>
2021-11-29 18:11:30 -0500

    Mapping over the Api

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,72 @@
+// import PetList from "./PetList";
+// import { Component } from "react";
+// import "./Employee.css";
+
+// class Employee extends Component {
+// 	constructor() {
+// 		super();
+// 		this.State = {
+// 			employeData: "",
+// 		};
+// 	}
+
+// 	componentDidMount() {
+// 		console.log("Did Mount");
+// 		this.employeeData();
+// 	}
+
+// 	employeeData = () => {
+// 		fetch("https://pursuit-veterinarian.herokuapp.com/api/employees")
+// 			.then((res) => {
+// 				return res.json();
+// 			})
+// 			.then((data) => {
+// 				console.log(data);
+// 				this.setState({
+// 					employeData: data,
+// 				});
+// 			});
+// 	};
+
+// 	render() {
+// 		let mapping = this.state.employeData.map((data) => {
+// 			return (
+// 				<div key={data.id}>
+// 					<h3>
+// 						{data.preflix}
+// 						{data.firstName}
+// 						{data.lastName}
+// 						{data.postfix}
+// 					</h3>
+// 					<h4>{data.title}</h4>
+// 				</div>
+// 			);
+// 		});
+
+// 		return (
+// 			<article className="employee">
+// 				<div>{mapping}</div>
+// 				<button>Show Pets</button>
+// 				<PetList />
+// 			</article>
+// 		);
+// 	}
+// }
+
+// export default Employee;
+
 import PetList from "./PetList";
 import "./Employee.css";
 
 export const Employee = () => {
-  return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
-  );
+	return (
+		<article className="employee">
+			<h3>Staff Member Name</h3>
+			<h4>Staff Member Title</h4>
+			<button>Show Pets</button>
+			<PetList />
+		</article>
+	);
 };
 
 export default Employee;
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -2,15 +2,36 @@ import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
-};
+class EmployeeList extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			dataEmployee: [],
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://pursuit-veterinarian.herokuapp.com/api/employees")
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				this.setState({ dataEmployee: data });
+			});
+	}
+
+	render() {
+		const mappingEmployee = this.state.dataEmployee.map((each) => {
+			return <Employee each={each} />;
+		});
+
+		return (
+			<main>
+				<h2>All Staff</h2>
+				<section className="employee-list">{mappingEmployee}</section>
+			</main>
+		);
+	}
+}
 
 export default EmployeeList;
