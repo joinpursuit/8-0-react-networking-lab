c4b76de06f5c7ffbb78c17b865fe0309d9af9444
Triane Peart <TrianePeart@gmail.com>
2021-11-29 13:00:12 -0500

    "First Commit" Draft one

--- src/index.js
+++ src/index.js
@@ -3,9 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+
+class index extends React.Component{
+  constructor (){
+    super();
+    this.state={
+      employee: ""
+    }
+  }
+  
+  
+  getInfo = () => {
+  fetch("https://vet-api-8-1.herokuapp.com/api/employees").then((response)=>{
+    return response.json()
+  }).then((info)=> {
+    this.setState({employee:info})
+  })
+  }
+}
+
+  ReactDOM.render(
+    <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+export default index
