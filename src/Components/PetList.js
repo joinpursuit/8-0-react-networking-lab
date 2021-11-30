fe1587ae673a1a35d6e6f2fbeef6b21f67f4fa91
Lakisha J <lakishajohnson@pursuit.org>
2021-11-29 21:54:38 -0500

    PetList updated

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,53 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
-};
+import React from "react";
+
+class PetList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      petApi: [],
+      // clicked: false,
+    };
+  }
+
+  // onClick = (event) => {
+    // event.preventDefault()
+  //   this.setState({
+  //     clicked: !this.state.clicked,
+  //   });
+  // };
+
+  componentDidMount() {
+    fetch(
+      `https://vet-api-8-1.herokuapp.com/api/pets?employeeId=${this.props.id}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          petApi: data,
+        });
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  }
+
+  render() {
+    const displayPets = this.state.petApi.map((pet, i) => {
+      return pet.name;
+    }).join(", ");
+    // console.log(displayPets)
+    
+    return (
+      <aside className="pets-list">
+        {displayPets.length > 0 ? <p>
+          {displayPets}
+        </p> : <p>No pets available</p>}
+      </aside>
+    );
+  }
+}
 
 export default PetList;
