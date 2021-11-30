b16cc8573a64a09c88cfc1c2bbdd3433ca422732
rae charles <r_charles87@yahoo.com>
2021-11-30 00:24:51 -0500

    finished employee data

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,62 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { Component } from "react";
 
-export const Employee = () => {
-  return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
-  );
+class Employee extends Component  {
+  constructor(){
+    super()
+    this.state={
+      employeeData: []
+    }
+
+
+  }
+
+  componentDidMount(){
+    this.handleEmployeeData()
+  }
+  
+  handleEmployeeData = () => {
+    fetch("https://pursuit-veterinarian.herokuapp.com/api/employees")
+    .then((res)=>{
+      return res.json();
+    }).then((data)=>{
+      this.setState({
+        employeeData: data
+      });
+    });
+    
+  }
+
+
+  handleShowPets = () =>{
+    return(
+      console.log("Trigger")
+      )
+    }
+    
+    render(){
+      
+      
+      let employeeDisplay = this.state.employeeData.map((employee)=>{
+        const { firstName, lastName, prefix, postfix, title, id} = employee
+        return (
+          <article className="employee">
+          <h3>{prefix} {firstName} {lastName}, {postfix}</h3>
+          <h4>{title}</h4>
+          <button onClick={this.handleShowPets}>Show Pets</button>
+          {/* <PetList id={id} handleShowPets={this.handleShowPets}/> */}
+        </article>
+      )
+    })
+    
+    
+    return (
+      <div>
+        {employeeDisplay}
+      </div> 
+    );
+  }
 };
 
 export default Employee;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,56 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
+import { Component } from "react";
+
+
+
+
+
+class PetList extends Component {
+  constructor(){
+    super()
+    this.state={
+      petArr:[]
+    }
+  }
+  componentDidMount(){
+    fetch("https://pursuit-veterinarian.herokuapp.com/api/pets")
+    .then((res)=>{
+      return res.json();
+    }).then((data)=>{
+      this.setState({
+        petArr: data
+      })
+    })
+  }
+
+
+handlePetDisplay=()=>{
+  this.state.petArr.forEach((pet)=>{
+    if(pet.employeeId === this.props.id){
+      return (
+        <p>{pet.name}</p> 
+      )
+    };
+  });
+}
+
+
+  render(){
+    console.log(this.props.id)
+
+    let petDisplay = this.state.petArr.map((pet)=>{
+      if(pet.employeeId === this.props.id){
+        return (
+          <p>{pet.name},</p>
+        )
+      }
+    });
+    
+    return (
+      <aside className="pets-list">
+          {petDisplay}
+        </aside> 
+    );
+  }
 };
 
 export default PetList;
