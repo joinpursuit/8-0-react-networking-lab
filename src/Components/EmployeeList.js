786e3dece0ee7e1c2249eeafcdc690be2f736ff3
CarlosAMoran <Carlosamoran18@gmail.com>
2021-11-30 10:01:34 -0500

    list employee done

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -2,15 +2,29 @@ import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export const EmployeeList = () => {
+export default class EmployeeList extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      listemployees: [],
+    }
+  }
+  componentDidMount() {
+    fetch("https://vet-api-8-1.herokuapp.com/api/employees")
+    .then(response => response.json())
+    .then(obj => {
+      this.setState({listemployees: [...obj]})
+    })
+    .catch(console.log);
+  }
+  render(){
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {this.state.listemployees.map((el, i)=> <Employee key={i} employee={el}/>)}
       </section>
     </main>
   );
+ }
 };
-
-export default EmployeeList;
