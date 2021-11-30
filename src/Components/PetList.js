0b44a71860a5d5bd17465af066da44e172545bff
Fatema Kabir <fatema.kabir@pursuit.org>
2021-11-29 21:07:34 -0500

    display-pet

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,40 @@
-export const PetList = () => {
+import { Component } from "react";
+
+class PetList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      petData: [],
+    };
+  }
+  componentDidMount() {
+    fetch(
+      `https://vet-api-8-1.herokuapp.com/api/pets?employeeId=${this.props.id}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ petData: data });
+      })
+      .catch((err) => console.log(err.message));
+  }
+
+  render() {
+    const { petData } = this.state;
+    const petList = petData.map((pet) => {
+      return <>{pet.name} </>;
+    });
     return (
       <aside className="pets-list">
-        <p>No pets listed for this employee.</p>
+        {petList.length > 0 ? (
+          <p>{petList}</p>
+        ) : (
+          <p>No pets listed for this employee.</p>
+        )}
       </aside>
     );
-  };
-  
-  export default PetList;
+  }
+}
+
+export default PetList;
