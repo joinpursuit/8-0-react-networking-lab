4e80e63b4933779bb56aa1e4d86c69180d9991fd
salrey <tekdehn@gmail.com>
2021-11-29 22:00:10 -0500

    Have components use react networking/external API

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,11 @@
-export const PetList = () => {
+const PetList = (props) => {
+  const { pets } = props
+  // console.log(pets)
+  const petList = pets.length ? pets.map((pet) => pet.name).join(", ") : <p>No pets listed for this employee.</p>
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {petList}
     </aside>
   );
 };
