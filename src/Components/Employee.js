3396ca4d71507c40b7017399b5d0978146d3e756
kisublee <yigi2021@gmail.com>
2021-11-29 23:47:19 -0500

    Confused.

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,58 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import React from "react";
 
-export const Employee = () => {
-  return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
+class Employee extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      pet: null,
+      click: false,
+    }
+    console.log("Under Construction for Pet")
+  }
+
+  // componentDidMount(){
+
+  //   fetch("https://vet-api-8-1.herokuapp.com/api/pets").then((response) => response.json()).then((data) => {
+
+  //   this.setState({
+  //     pet: data
+  //   })  
+
+  //   }).catch(console.log)
+
+  // }
+
+  test = () => {
+    if (this.state.click) {
+      this.setState({
+        click: false,
+      })
+    } else {
+    this.setState({
+      click: true,
+    })
+  }
+  }
+
+
+  render() {
+
+        return (
+         this.props.person === null ? <h3>Updating</h3> : this.props.person.map((each, i) => {
+          return (
+
+             <article className="employee" key = {i}>
+               <h3>{`${each.prefix && each.prefix} ${each.firstName} ${each.lastName}${each.postfix && ", "+ each.postfix} `}</h3>
+               <h4>{each.title}</h4>
+               <button onClick ={this.test}>Show Pets</button>
+              <PetList each = {each} pets = {this.state.pet}/>
+             </article>
+      )
+    })
   );
+    }
 };
 
 export default Employee;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,70 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
-};
+// export const PetList = (props) => {
+
+
+  // console.log(props.employees)
+  // console.log(props.pets)
+
+  // const displayPets = props.pets !== null && props.pets.map((pet) => {
+
+  //   return (
+  //     pet.employeeId === props.each.id && <p>{pet.name}</p>
+  //   )
+  // })
+
+  //     console.log(displayPets)
+  //     return (
+  //           <aside className="pets-list">
+  //               {displayPets.every((each) => each === false) ? <p>No pets listed for this employee.</p> :<p>{displayPets}</p>}
+  //           </aside>
+
+    // props.pets === null ? <h2>test</h2> : props.pets.map((pet) => {
+    //   return (pet.employeeId  === props.each.id && <aside className="pets-list">
+    //   <p>{pet.employeeId  === props.each.id ? pet.name : "No pets listed for this employee."}</p>
+    //    </aside>
+    //   )
+    // })
+//   );
+// };
+
+// export default PetList;
+
+
+import React from "react";
+
+class PetList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pets: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch(`https://vet-api-8-1.herokuapp.com/api/pets?employeeId=${this.props.each.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ 
+          pets: data,
+        });
+      })
+      .catch((error) => {
+        console.log("Error fetching pets");
+      });
+  }
+
+
+  render() {
+    const displayPets = this.state.pets !== null && this.state.pets.map((pet) => {
+      return <p>{pet.name}</p>
+    })
+    console.log(displayPets)
+
+    return (
+      <aside className="pets-list">
+        {displayPets.length > 0 ? <p>{displayPets}</p> : <p>No pets listed for this employee.</p>}
+      </aside>
+    );
+  }
+}
 
 export default PetList;
