5d0474fa40198dc8c91e00069353e6964c807f5b
Lakisha J <lakishajohnson@pursuit.org>
2021-11-29 21:53:56 -0500

    EmployeeList componentDidMount

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -2,15 +2,46 @@ import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
-};
+class EmployeeList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      apiArray: [],
+    };
+  }
+
+
+  componentDidMount() {
+    fetch("https://vet-api-8-1.herokuapp.com/api/employees")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          apiArray: data,
+        });
+      }).catch(error => {
+        console.log("Error: ", error)
+      })
+  }
+
+  render() {
+    const results = this.state.apiArray.map((staff, i) => {
+      return (
+        <Employee key={i} person={staff}
+        />
+      );
+    });
+
+    return (
+      <main>
+        <h2>All Staff</h2>
+        <section className="employee-list">{results}
+        </section>
+      </main>
+    );
+  }
+}
 
 export default EmployeeList;
