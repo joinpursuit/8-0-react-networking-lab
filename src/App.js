89d0958d9713d93f73e698a44495c2e300bd437e
Sara Perkaj <saraperkaj@pursuit.org>
2021-11-30 09:53:57 -0500

    done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
+import { render } from "@testing-library/react";
 
 const App = () => {
   return (
--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,28 @@
 import PetList from "./PetList";
 import "./Employee.css";
 
-export const Employee = () => {
-  return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
-  );
-};
-
+class Employee extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      clicked: false,
+    };
+  }
+  render() {
+    return (
+      <article className=“employee”>
+        <h3>
+          Staff Member Name: {this.props.eachPerson.prefix}{” “}
+          {this.props.eachPerson.firstName} {this.props.eachPerson.lastName},{” “}
+          {this.props.eachPerson.postfix}
+        </h3>
+        <h4>Staff Member Title</h4>
+        <button onClick={() => this.setState({ clicked: !this.state.clicked })}>
+          Show Pets
+        </button>
+        {this.state.clicked ? <PetList id={this.props.eachPerson.id} /> : null}
+      </article>
+    );
+  }
+}
 export default Employee;
