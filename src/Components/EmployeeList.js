95af939ef643996d1ac8c386bdb77144f7ac465a
vanessa conti <vanessaconti@pursuit.org>
2021-11-30 01:32:32 -0500

    passing tests finally

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -2,15 +2,39 @@ import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
-};
+class EmployeeList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://vet-api-8-1.herokuapp.com/api/employees")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTimeout(() => {
+          console.log("Component Did Mount");
+          this.setState({ people: data });
+        }, 3000);
+      });
+  }
+
+  render() {
+    const eachPersonForReal = this.state.people.map((eachPerson) => {
+      return <Employee eachPerson={eachPerson} />;
+    });
+
+    return (
+      <main>
+        <h2>All Staff</h2>
+        <section className="employee-list">{eachPersonForReal}</section>
+      </main>
+    );
+  }
+}
 
 export default EmployeeList;
