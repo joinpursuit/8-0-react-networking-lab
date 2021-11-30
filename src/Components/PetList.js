860d534961125d9a5afd66a25fa40c34952a6442
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2021-11-29 23:25:31 -0500

    React-Networking-Lab

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,16 @@
-export const PetList = () => {
+const PetList = (props) => {
+  const { pets } = props;
+
+  const petList = pets.length ? (
+    pets.map((pet) => pet.name).join(", ")
+  ) : (
+    <p>No pets listed for this employee.</p>
+  );
+
   return (
     <aside className="pets-list">
       <p>No pets listed for this employee.</p>
+      {petList}
     </aside>
   );
 };
