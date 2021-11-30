9eb1b91ba55fed0de72eed258abffb85d10e7062
stephanie lucano <stephaniemarielucano@gmail.com>
2021-11-29 19:55:06 -0500

    Populated the employee card with name and title and set up a ternary operator to show the PetList component when the button is clicked

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,34 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { Component } from "react";
 
-export const Employee = () => {
-  return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
-  );
+class Employee extends Component {
+  constructor() {
+    super()
+    this.state = {
+      clicked: false,
+    }
+  }
+
+  render() {
+    const {id, firstName, lastName, prefix, postfix, title} = this.props.eachPerson
+    return (
+      <article className="employee">
+        <h3>{prefix} {firstName} {lastName} {postfix ? ", " + postfix : null}</h3>
+        <h4>{title}</h4>
+        <button 
+          onClick={() => {
+            this.setState({
+              clicked: !this.state.clicked,
+            })
+          }}
+        >
+          Show Pets
+        </button>
+        {this.state.clicked ? <PetList id={id} /> : null}
+      </article>
+    );
+  }
 };
 
 export default Employee;
