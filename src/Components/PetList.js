6931cd73124f5941ff0bb5aee9b958b96933f15a
Angie G <angieg@Angies-MBP.fios-router.home>
2021-11-29 19:36:17 -0500

    PetList File

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,16 +1,41 @@
-import React from "react";
-import Employee from "./Employee";
-import "./EmployeeList.css";
+import { Component } from "react";
 
-export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
-};
+class PetList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pets: [],
+    };
+  }
 
-export default EmployeeList;
+  componentDidMount() {
+    fetch("https://vet-api-8-1.herokuapp.com/api/pets")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ pets: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const arr = [];
+    this.state.pets.map((pet) => {
+      this.props.id === pet.employeeId && arr.push(pet.name);
+      return arr;
+    });
+
+    return (
+      <aside className="pets-list">
+        <p>
+          {arr.length !== 0
+            ? arr.join(", ")
+            : "No pets listed for this employee."}
+        </p>
+      </aside>
+    );
+  }
+}
+
+export default PetList;
