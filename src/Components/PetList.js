a4de180c84d606a4a1ad94300fad4f99cdb7f8fe
Taylor W <taylorwilliams@pursuit.org>
2021-11-29 23:19:32 -0500

    Completed Lab

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,42 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
+import { Component } from "react";
+
+class PetList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      pets: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://vet-api-8-1.herokuapp.com/api/pets")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ pets: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
+  render() {
+    const newArr = [];
+    this.state.pets.map((pet) => {
+      this.props.id === pet.employeeId && newArr.push(pet.name);
+      return newArr;
+    });
+    
+    return (
+      <aside className="pets-list">
+        <p>
+          {newArr.length !== 0 ?
+          newArr.join(",")
+          : "No pets listed for this employee."}
+          </p>
+      </aside>
+    );
+  }
 };
 
 export default PetList;
