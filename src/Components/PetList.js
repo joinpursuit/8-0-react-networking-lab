0b5265ec2a0ea1d7241d24724d3799b2499279f8
CarlosAMoran <Carlosamoran18@gmail.com>
2021-11-30 10:11:15 -0500

    petlist done

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,27 @@
-export const PetList = () => {
+import React from "react";
+
+export default class PetList extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      pets: [],
+    }
+  }
+  componentDidMount(){
+    const {eid} = this.props
+    fetch(`https://vet-api-8-1.herokuapp.com/api/pets?employeeId=${eid}`)
+    .then(response => response.json())
+    .then(obj => {
+      this.setState({pets: [...obj]})
+    })
+    .catch(console.log)
+  }
+  render(){
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {this.state.pets.map(({name})=> name).join(", ") || <p>No pets listed for this employee.</p>}
     </aside>
   );
+  }
 };
 
-export default PetList;
