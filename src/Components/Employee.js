a2b28084bfd83bb7b366a38dda8116b970c02021
Name Ping Louie <namepinglouie@gmail.com>
2021-12-01 11:51:30 -0500

    finally completed lab

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,42 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { Component } from "react";
 
-export const Employee = () => {
-  return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
-  );
-};
+class Employee extends Component {
+  constructor() {
+    super();
+    this.state = {
+      petList: [],
+    }
+  }
+  concatStaffName = () => {
+    const {firstName, lastName, prefix, postfix} = this.props.staff;
+    let fullName = `${firstName} ${lastName}`;
+    if(prefix) fullName = `${prefix} ${fullName}`;
+    else if(postfix) fullName = `${fullName}, ${postfix}`;
+    return fullName;
+  }
+
+  showPet = (id) => {
+    fetch(`https://pursuit-veterinarian.herokuapp.com/api/pets?employeeId=${id}`)
+    .then(res => res.json())
+    .then((petData) => {
+      this.setState({
+        petList: petData,
+      })
+    })
+  }
+
+  render() {
+    return(
+      <article className="employee">
+       <h3>{this.concatStaffName()}</h3>
+       <h4>{this.props.staff.title}</h4>
+       <button onClick={()=>this.showPet(this.props.staff.id)}>Show Pets</button>
+       <PetList petList = {this.state.petList} />
+     </article>
+    )
+  }
+}
 
 export default Employee;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,20 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
-};
+import { Component } from "react";
+
+class PetList extends Component {
+  joinPetNames = () => {
+    if(!this.props.petList.length) return "No pets listed";
+    else {
+      return this.props.petList.map((pet)=> {
+        return pet.name
+      }).join(", ")
+    } 
+  }
+
+  render() {
+    return (
+      <aside className="pets-list">{this.joinPetNames()}</aside>
+    )
+  }
+}
 
 export default PetList;
