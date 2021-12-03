e5d996b500163202452e9c78a17f4917a844c063
“Lili <lilyhuang114classb@gmail.com>
2021-12-03 13:14:55 -0500

    Fixed the problem which is if statement should have the length of showPets.

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -6,10 +6,10 @@ class PetList extends React.Component {
 
   render(){
     let {showPets, havePets} = this.props;
-    
+    let associatedPets = havePets && showPets.length!==0 ? showPets.join(", ") : "No pets listed";
     return (
       <aside className="pets-list">
-        <p>{havePets && showPets ? showPets.join(", ") : "No pets listed"}</p>
+        <p>{associatedPets}</p>
       </aside>
     );
   }
