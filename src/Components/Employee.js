23e3091a5836397fb5f45e93f3965b24ff1154e8
JuHaoChen1997 <juhaochen97@gmail.com>
2022-05-25 12:54:55 -0400

    pass show staff name, include prefixes, include postfixes tests

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,10 +1,15 @@
 import PetList from "./PetList";
 import "./Employee.css";
 
-export const Employee = () => {
+export const Employee = (props) => {
+  let { id, firstName, lastName, prefix, postfix } = props.employeeData;
+  if (postfix) {
+    postfix = ", " + postfix;
+  }
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
+      <h3>{`${prefix} ${firstName} ${lastName}${postfix}`}</h3>
       <h4>Staff Member Title</h4>
       <button>Show Pets</button>
       <PetList />
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -2,15 +2,46 @@ import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
-};
+class EmployeeList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      employees: null,
+    };
+  }
+
+  updateEmployeesData = () => {
+    fetch("https://serene-tundra-77911.herokuapp.com/api/employees")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ employees: data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  componentDidMount() {
+    this.updateEmployeesData();
+  }
+
+  render() {
+    const { employees } = this.state;
+
+    return (
+      <main>
+        <h2>All Staff</h2>
+        <section className="employee-list">
+          {employees &&
+            employees.map((employee) => {
+              return <Employee key={employee.id} employeeData={employee} />;
+            })}
+        </section>
+      </main>
+    );
+  }
+}
 
 export default EmployeeList;
