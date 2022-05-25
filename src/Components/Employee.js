2899b3cf5ea4bb5c4137d080334927f802ead26a
meera-ramesh19 <meeraramesh19@outlook.com>
2022-05-25 13:20:09 -0400

    lab completed

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,37 @@
-import PetList from "./PetList";
-import "./Employee.css";
+import PetList from './PetList';
+import './Employee.css';
+import React, { Component } from 'react';
 
-export const Employee = () => {
-  return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
-  );
-};
+class Employee extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showPets: false,
+    };
+  }
+
+  showOrHide = () => {
+    this.setState({ showPets: !this.state.showPets });
+  };
+
+  render() {
+    const { prefix, firstName, lastName, title, postfix, id } =
+      this.props.employee;
+    console.log('props', this.props.employee);
+    return (
+      <article className='employee'>
+        <h3>
+          {[prefix, firstName, lastName].join(' ')}
+          {postfix && ', ' + postfix}
+        </h3>
+        <h4>{title}</h4>
+        <button onClick={this.showOrHide}>
+          {!this.state.showPets ? 'Show Pets' : 'Hide Pets'}
+        </button>
+        {this.state.showPets && <PetList eid={id} />}
+      </article>
+    );
+  }
+}
 
 export default Employee;
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,16 +1,56 @@
-import React from "react";
-import Employee from "./Employee";
-import "./EmployeeList.css";
+import React, { Component } from 'react';
+import Employee from './Employee.js';
+import './EmployeeList.css';
 
-export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
-};
+class EmployeeList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      employeeList: [],
+    };
+  }
+
+  //function to fetch apiData
+  // updateEmployeeURL = () => {
+  //   console.log('updating the employee url');
+  //   fetch('http://serene-tundra-77911.herokuapp.com/api/employees')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       if (data.status === 'success') {
+  //         this.setState({
+  //           employeeList: [...data],
+  //         });
+  //       }
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
+
+  componentDidMount() {
+    // this.updateEmployeeURL();
+    fetch('http://serene-tundra-77911.herokuapp.com/api/employees')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // if (data.status === 'success') {
+          this.setState({
+            employeeList: [...data],
+          });
+        // }
+      })
+      .catch((err) => console.log(err));
+  }
+  render() {
+    return (
+      <main>
+        <h2>All Staff</h2>
+        <section className='employee-list'>
+          {this.state.employeeList.map((emp, idx) => (
+            <Employee key={idx} employee={emp} />
+          ))}
+        </section>
+      </main>
+    );
+  }
+}
 
 export default EmployeeList;
