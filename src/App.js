7aaa28acebd8683d693c03f74666fe09c65d204b
named.josie <named.josie@gmail.com>
2022-05-26 00:16:56 -0400

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
 
-const App = () => {
-  return (
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      EMPLOYEES_URL: [],
+
+    }
+  }
+  fetchdata = () => {
+    fetch('https://serene-tundra-77911.herokuapp.com/api/employees')
+    .then((res) => res.json())
+    .then((employees) => {
+    this.setState({
+      EMPLOYEES_URL: employees,
+    });
+    });
+    }; 
+    render() {
+      const { EMPLOYEES_URL } = this.state;
+        return (
     <>
       <NavBar />
-      <EmployeeList />
+      <EmployeeList employees={EMPLOYEES_URL}/>
     </>
   );
 };
-
+}
 export default App;
