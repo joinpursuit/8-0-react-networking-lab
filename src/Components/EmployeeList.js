e336baeecd4a273d3f8641e03a4cf9ddececd40a
Tom LaTulipe <tlatulipe@gmail.com>
2022-05-25 20:01:37 -0400

    trying something

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -7,16 +7,21 @@ import "./EmployeeList.css";
 export default class EmployeeList extends Component {
   constructor() {
     super();
+    this.state = {
+      employeeList: []
+    }
   }
 
   listEmployees = () => {
     fetch("https://vet-lab-8-4.herokuapp.com/api/employees")
       .then((employees) => employees.json())
       .then((employees) => console.log(employees))
-      .then((employees) => this.testEmployees());
+      .then((employees) => {
+        for (let employee of employees) {
+          this.state.employeeList.push(employee);
+        }
+      });
   };
-
-  testEmployees = () => this.listEmployees();
 
   componentDidMount() {
     this.listEmployees();
@@ -26,7 +31,7 @@ export default class EmployeeList extends Component {
       <main>
         <h2>All Staff</h2>
         <section className="employee-list">
-          <Employee employee={this.listEmployees()}/>
+          <Employee />
         </section>
       </main>
     );
