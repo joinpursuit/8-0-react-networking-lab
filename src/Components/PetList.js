268fd747392326f962f9461ba49ce8f0ea5a0d3e
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-05-28 00:26:07 -0400

    Done.

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,41 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
-};
+import React from "react";
 
+import { Component } from "react";
+
+class PetList extends Component {
+  constructor() {
+    super();
+    this.state = { petsData: [] };
+  }
+
+  componentDidMount() {
+    fetch(
+      `https://pursuit-veterinarian.herokuapp.com/api/pets?employeeId=${this.props.theId}`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((Data) => {
+        this.setState({
+          petsData: Data,
+        });
+      });
+  }
+
+  render() {
+    let allPets = this.state.petsData.map((each) => {
+      return <>{each.name} </>;
+    });
+
+    return (
+      <aside className="pets-list">
+        {allPets.length > 0 ? (
+          <p>{allPets}</p>
+        ) : (
+          <p>No pets listed for this employee.</p>
+        )}
+      </aside>
+    );
+  }
+}
 export default PetList;
