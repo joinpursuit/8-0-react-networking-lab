a5e38ba44de68697aff7081f765a18f2800c75a9
Matt M <Afrodesiacc@gmail.com>
2022-05-29 18:29:05 -0400

    Completed

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,16 @@
-export const PetList = () => {
+import React from 'react'
+
+
+class PetList extends React.Component {
+  render() {
+    console.log(this.props.pets)
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {!this.props.pets.length && <p>No pets listed for this employee.</p>}
+      {this.props.pets.length > 0 && <p>{this.props.pets.map((pet) => { return pet.name}).join(', ')}</p>}
     </aside>
   );
+  }
 };
 
 export default PetList;
