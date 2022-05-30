7581017480db6240dbf04d194177cd53cb02ce9b
lchen6283 <lchen6283@gmail.com>
2022-05-30 11:26:13 -0400

    react-network-lab

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
 
-const App = () => {
-  return (
-    <>
-      <NavBar />
-      <EmployeeList />
-    </>
-  );
-};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      empArr: [],
+    };
+  }
 
+  getEmp = () => {
+    fetch("https://serene-tundra-77911.herokuapp.com/api/employees")
+      .then((response) => response.json())
+      .then((employees) => this.setState({ empArr: employees }));
+  };
+
+  componentDidMount() {
+    this.getEmp();
+  }
+
+  render() {
+    const { empArr } = this.state;
+    return (
+      <>
+        <NavBar />
+        <EmployeeList empArr={empArr} />
+      </>
+    );
+  }
+}
 export default App;
