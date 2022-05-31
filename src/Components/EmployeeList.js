959323dee3ac89a63271a7a7f231513cf2c76a48
Derek <plutus.derekpeterson@gmail.com>
2022-05-30 23:05:31 -0400

    React Networking Lab Done

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,16 +1,38 @@
-import React from "react";
-import Employee from "./Employee";
-import "./EmployeeList.css";
+import React from 'react';
+import Employee from './Employee';
+import './EmployeeList.css';
 
-export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
-};
+class EmployeeList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      employees: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://serene-tundra-77911.herokuapp.com/api/employees')
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          employees: [...data],
+        })
+      )
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    return (
+      <main>
+        <h2>All Staff</h2>
+        <section className='employee-list'>
+          {this.state.employees.map((employee, index) => (
+            <Employee key={index} employee={employee} />
+          ))}
+        </section>
+      </main>
+    );
+  }
+}
 
 export default EmployeeList;
