c06ddbe22c5204d5bcb646a697671353c45020f3
TaryneLeach <taryneleach@gmail.com>
2022-05-30 22:13:41 -0400

    submitting

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,64 @@
-import PetList from "./PetList";
-import "./Employee.css";
+import PetList from './PetList';
+import './Employee.css';
+import React from 'react';
 
-export const Employee = () => {
-  return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
-  );
-};
+export class Employee extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			pets: [],
+			toggle: false,
+		};
+	}
+
+	getEmployeePets = () => {
+		fetch('https://serene-tundra-77911.herokuapp.com/api/pets')
+			.then((response) => response.json())
+			.then((json) => {
+				this.setState({
+					pets: json,
+				});
+			});
+	};
+
+	componentDidMount() {
+		this.getEmployeePets();
+	}
+
+	toggle = () => {
+		const { toggle } = this.state;
+		this.setState({ toggle: !toggle });
+	};
+	render() {
+		const { employees } = this.props;
+		const { firstName, lastName, prefix, postfix, title } = employees;
+		const { toggle, pets } = this.state;
+		let name = null;
+
+		// tried using a ternary but may have gotten the syntax wrong
+
+		if (postfix) {
+			name = (
+				<p>
+					{prefix} {firstName} {lastName}, {postfix}
+				</p>
+			);
+		} else {
+			name = (
+				<p>
+					{prefix} {firstName} {lastName}
+				</p>
+			);
+		}
+		return (
+			<article className='employee'>
+				<h3>{name}</h3>
+				<h4>{title}</h4>
+				<button onClick={this.toggle}>Show Pets</button>
+				{toggle ? <PetList pets={pets} employees={employees} /> : null}
+			</article>
+		);
+	}
+}
 
 export default Employee;
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,16 +1,18 @@
-import React from "react";
-import Employee from "./Employee";
-import "./EmployeeList.css";
+import React from 'react';
+import Employee from './Employee';
+import './EmployeeList.css';
 
 export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
+	return (
+		<main>
+			<h2>All Staff</h2>
+			<section className='employee-list'>
+				{employeeArr.map((employee) => {
+					return <Employee employee={employee} key={employee.id} />;
+				})}
+			</section>
+		</main>
+	);
 };
 
 export default EmployeeList;
