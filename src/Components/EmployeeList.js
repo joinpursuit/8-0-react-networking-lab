247db80796c2c8ef3b9d6f149f8468a9ada7afe2
JC-MT <jancarlosmatias@gmail.com>
2022-05-31 09:47:53 -0400

    lab completed

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -2,12 +2,17 @@ import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export const EmployeeList = () => {
+export const EmployeeList = ({pets, employees}) => {
+
+  const allEmployees = employees.map((employee) => {
+    return <Employee key={employee.id} pets={pets} employee={employee}/>
+  })
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {allEmployees}
       </section>
     </main>
   );
