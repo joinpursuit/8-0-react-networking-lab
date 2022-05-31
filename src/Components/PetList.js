8ebab69d18586081bacded7ce5fc6da7a088bb42
Jonathan Scheiber <jon@scheiber.me>
2022-05-31 00:55:56 -0400

    update PetList.js

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,21 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
-};
+import { Component } from "react";
+
+class PetList extends Component {
+  joinPetNames = () => {
+    if (!this.props.petList.length) {
+      return "No pets listed for this employee.";
+    } else {
+      return this.props.petList
+        .map((pet) => {
+          return pet.name;
+        })
+        .join(", ");
+    }
+  };
+
+  render() {
+    return <aside className="pets-list">{this.joinPetNames()}</aside>;
+  }
+}
 
 export default PetList;
