d2febc42e4e27076958ccd6948e7d8d2cb573fa4
Jonathan Scheiber <jon@scheiber.me>
2022-05-31 00:49:41 -0400

    update EmployeeList.js

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,16 +1,41 @@
-import React from "react";
+import { Component } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
-};
+class EmployeeList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      staffList: [],
+    };
+  }
+
+  showStaff = () => {
+    fetch(`https://serene-tundra-77911.herokuapp.com/api/employees`)
+      .then((res) => res.json())
+      .then((staffData) => {
+        this.setState({
+          staffList: staffData,
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.showStaff();
+  }
+
+  render() {
+    const staffArray = this.state.staffList.map((staff) => {
+      return <Employee key={staff.id} staff={staff} />;
+    });
+
+    return (
+      <main>
+        <h2>All Staff</h2>
+        <section className="employee-list">{staffArray}</section>
+      </main>
+    );
+  }
+}
 
 export default EmployeeList;
