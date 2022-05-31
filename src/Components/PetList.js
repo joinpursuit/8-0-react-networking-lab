314eeec67e4fa874d06830984c7e2e8f00dffed5
Spencer Simon <spencersimon@pursuit.org>
2022-05-31 18:04:29 -0400

    React Networking

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,41 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
-};
+import { Component } from "react";
+
+class PetList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      petInfo: [],
+    };
+  }
+
+  componentDidMount = () => {
+    fetch(
+      `https://vet-api-8-1.herokuapp.com/api/pets?employeeId=${this.props.id}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          petInfo: data,
+        });
+      });
+  };
+
+  render() {
+    const petName = this.state.petInfo.map((pet, id) => {
+      return <div key={id}>{pet.name}</div>;
+    });
+    return (
+      <aside>
+        {petName.length > 0 ? (
+          <div>{petName}</div>
+        ) : (
+          <p>No pets listed for this employee.</p>
+        )}
+      </aside>
+    );
+  }
+}
 
 export default PetList;
