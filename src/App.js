890744f5e9c04551b53519750a5972a4e776e8f3
Edwin Codrington <edwincodrington@gmail.com>
2022-05-30 21:24:11 -0400

    Content is there. My Hide/Show button not working however..

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
 
+const BASE_URL = "https://serene-tundra-77911.herokuapp.com/";
+
+
 const App = () => {
-  return (
-    <>
-      <NavBar />
-      <EmployeeList />
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<EmployeeList url={BASE_URL} />
+		</>
+	);
 };
 
 export default App;
