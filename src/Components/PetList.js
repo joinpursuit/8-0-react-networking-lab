948a3454f03b85b04557882821b106494e3cde59
Tom LaTulipe <tlatulipe@gmail.com>
2022-05-31 21:33:39 -0400

    PetList refactored? Buttons not working

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,36 +1,40 @@
 import { Component } from "react/cjs/react.production.min";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default class PetList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      petList: this.props.pets,
-    };
-  }
+const PetList = (props) => {
+  const [petList, setpetList] = useState(props.pets);
 
-  render() {
-    return (
-      <aside className="petList">
-        <div>
-          <br></br>
-          {this.state.petList.length ? (
-            this.state.petList.map((pet) => {
-              return (
-                <>
-                  {pet.name}
-                  {this.state.petList.indexOf(pet) ===
-                  this.state.petList.length - 1
-                    ? null
-                    : ", "}
-                </>
-              );
-            })
-          ) : (
-            <h4>No pets listed</h4>
-          )}
-        </div>
-      </aside>
-    );
-  }
-}
+  return (
+    <aside className="petList">
+      <div>
+        <br></br>
+        {petList.length ? (
+          petList.map((pet) => {
+            return (
+              <>
+                {pet.name}
+                {petList.indexOf(pet) === petList.length - 1 ? null : ", "}
+              </>
+            );
+          })
+        ) : (
+          <h4>No pets listed</h4>
+        )}
+      </div>
+    </aside>
+  );
+};
+
+export default PetList;
+
+//export default class PetList extends Component {
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     petList: this.props.pets,
+//   };
+// }
+
+// render() {
+//}
+//}
