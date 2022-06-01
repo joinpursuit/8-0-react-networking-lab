db7745ef629f8802ad96388b9c605543ea310edc
Tom LaTulipe <tlatulipe@gmail.com>
2022-05-31 21:18:00 -0400

    EmployeeList Refactored?

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,52 +1,56 @@
 //import { render } from "@testing-library/react";
-import React from "react";
-import { Component } from "react/cjs/react.production.min";
+import React, { useEffect, useState } from "react";
+//import { Component, useState } from "react/cjs/react.production.min";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export default class EmployeeList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      employeeList: [],
-    };
-  }
+const EmployeeList = ({ employee }) => {
+  const [employeeList, setEmployeeList] = useState([]);
 
-  listEmployees = () => {
+  const listEmployees = () => {
     fetch("https://vet-lab-8-4.herokuapp.com/api/employees")
       .then((employees) => employees.json())
       .then((employees) => {
-        this.setState({
-          employeeList: employees,
-        });
+        setEmployeeList(employees);
+        // this.setState({
+        //   employeeList: employees,
+        // });
       })
       .catch((err) => alert("ERROR"));
   };
 
-  componentDidMount() {
-    this.listEmployees(); // To call the listEmployees as soon as component is loaded
-  }
+  useEffect(() => {
+    listEmployees();
+  }, []);
 
-  render() {
-    return (
-      <main>
-        <h2>All Staff</h2>
-        <section className="employee-list">
-          {this.state.employeeList.map((employee) => {
-            return (
-              <Employee
-                key={employee.id}
-                firstName={employee.firstName}
-                lastName={employee.lastName}
-                title={employee.title}
-                prefix={employee.prefix}
-                postfix={employee.postfix}
-                id={employee.id}
-              />
-            );
-          })}
-        </section>
-      </main>
-    );
-  }
-}
+  return (
+    <main>
+      <h2>All Staff</h2>
+      <section className="employee-list">
+        {employeeList.map((employee) => {
+          return (
+            <Employee
+              key={employee.id}
+              firstName={employee.firstName}
+              lastName={employee.lastName}
+              title={employee.title}
+              prefix={employee.prefix}
+              postfix={employee.postfix}
+              id={employee.id}
+            />
+          );
+        })}
+      </section>
+    </main>
+  );
+};
+
+export default EmployeeList;
+
+//export default class EmployeeList extends Component {
+// constructor() {
+//   super();
+//   this.state = {
+//     employeeList: [],
+//   };
+// }
