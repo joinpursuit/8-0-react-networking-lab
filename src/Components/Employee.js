ca6ce19bf2cec85ba2de178acd76641513443408
Tom LaTulipe <tlatulipe@gmail.com>
2022-05-31 21:27:17 -0400

    Employee.js refactored?

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,23 +1,18 @@
 import PetList from "./PetList";
 import "./Employee.css";
-import { Component } from "react/cjs/react.production.min";
+import React, { useEffect, useState } from "react";
 
-export default class Employee extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: this.props.firstName,
-      lastName: this.props.lastName,
-      title: this.props.title,
-      prefix: this.props.prefix,
-      postfix: this.props.postfix,
-      id: this.props.id,
-      petList: [],
-      isActive: false,
-    };
-  }
-
-  listPet = (id) => {
+const Employee = (props) => {
+  const [firstName, setfirstName] = useState(props.firstName);
+  const [lastName, setlastName] = useState(props.lastName);
+  const [title, settitle] = useState(props.title);
+  const [prefix, setprefix] = useState(props.prefix);
+  const [postfix, setpostfix] = useState(props.postfix);
+  const [id, setid] = useState(props.id);
+  const [petList, setpetList] = useState([]);
+  const [isActive, setisActive] = useState(false);
+  
+  const listPet = (id) => {
     fetch(`https://vet-lab-8-4.herokuapp.com/api/pets?employeeId=${id}`)
       .then((pets) => pets.json())
       .then((pets) => {
@@ -28,22 +23,48 @@ export default class Employee extends Component {
       })
       .catch((err) => console.log(err));
   };
+  
+  useEffect(() => {
+    console.log("UPDATING")
+  }, [props])
+  
+  return (
+    <article className="employee">
+      <h3>
+        {prefix} {firstName} {lastName}
+        {postfix ? `, ${postfix}` : null}
+        {/* {this.state.prefix} {this.state.firstName} {this.state.lastName}
+        {this.state.postfix ? `, ${this.state.postfix}` : null} */}
+      </h3>
+      <h4>{title}</h4>
+      {/* <h4>{this.state.title}</h4> */}
+      <button onClick={(e) => listPet(id)}>Show Pets</button>
+      {isActive ? <PetList pets={petList} /> : null}
+    </article>
+  );
+};
 
-  componentDidUpdate() {
-    console.log("HOLD ON");
-  }
+export default Employee
+//export default class Employee extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     firstName: this.props.firstName,
+  //     lastName: this.props.lastName,
+  //     title: this.props.title,
+  //     prefix: this.props.prefix,
+  //     postfix: this.props.postfix,
+  //     id: this.props.id,
+  //     petList: [],
+  //     isActive: false,
+  //   };
+  // }
 
-  render() {
-    return (
-      <article className="employee">
-        <h3>
-          {this.state.prefix} {this.state.firstName} {this.state.lastName}
-          {this.state.postfix ? `, ${this.state.postfix}` : null}
-        </h3>
-        <h4>{this.state.title}</h4>
-        <button onClick={(e) => this.listPet(this.state.id)}>Show Pets</button>
-        {this.state.isActive ? <PetList pets={this.state.petList} /> : null}
-      </article>
-    );
-  }
-}
+
+  // componentDidUpdate() {
+  //   console.log("HOLD ON");
+  // }
+
+//   render() {
+//   }
+// }
