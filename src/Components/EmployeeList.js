0c56fe9f53ce7ea0592b964d4570a0861054f788
Zane <zaneahmed4@gmail.com>
2022-05-31 21:58:10 -0400

    lab done

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
+const EmployeeList = () => {
+
+  const [employees, setEmployees] = useState([])
+
+  const getEmployees = () => {
+    fetch("https://vet-lab-8-4.herokuapp.com/api/employees")
+    .then(data => data.json())
+    .then(json => {
+      setEmployees(json);
+    })
+    .catch((error) => {
+      alert(error)
+    })
+  }
+
+  useEffect(()=>{
+    getEmployees()
+  }, []
+  )
+
+    return (
+      <main>
+        <h2>All Staff</h2>
+        <section className="employee-list">
+          {employees.map((employee) => <Employee employee={employee}/>)}
+        </section>
+      </main>
+    );
+
 };
 
 export default EmployeeList;
