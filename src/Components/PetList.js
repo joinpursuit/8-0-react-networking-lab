892ffde05a985afead60812402bb5436f399069e
Cesar Ortiz <ceazthemoment@aol.com>
2022-06-01 22:57:56 -0400

    lab networking

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,46 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
-};
+import react from "react";
+
+class PetList extends react.Component {
+  constructor() {
+    super();
+    this.state = {
+      listOfPets: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      `https://vet-api-8-1.herokuapp.com/api/pets?employeeId=${this.props.id}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTimeout(() => {
+          console.log("Component Did Mount");
+          this.setState({ listOfPets: data });
+        }, 1);
+      });
+  }
+
+  render() {
+    console.log(this.state.listOfPets);
+
+    const doctorsPet = this.state.listOfPets.map((eachPet) => {
+      return <p>{eachPet.name}</p>;
+    });
+    console.log(doctorsPet);
+
+    return (
+      <aside className="pets-list">
+        {doctorsPet.length > 0 ? (
+          <p>{doctorsPet}</p>
+        ) : (
+          <p>No pets listed for this employee.</p>
+        )}
+      </aside>
+    );
+  }
+}
 
 export default PetList;
