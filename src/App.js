1831bcae3ecc33bd48128db537f4417615a2d099
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-06-02 13:19:21 -0400

    Class Components switched to useState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
 
-// const App = () => {
-//   return (
-//     <>
-//       <NavBar />
-//       <EmployeeList />
-//     </>
-//   );
-// };
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <EmployeeList />
+    </>
+  );
+};
 
-// export default App;
+export default App;
 
-export default class App extends Component {
+// export default class App extends Component {
   
- render() {
-    return (
-      <>
-        <NavBar />
-        <EmployeeList />
-       </>
-    )
-  }
-}
+//  render() {
+//     return (
+//       <>
+//         <NavBar />
+//         <EmployeeList />
+//        </>
+//     )
+//   }
+// }
 
 
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,64 +1,85 @@
-import React, {Component} from "react";
+import React, { Component, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
+//     export default class EmployeeList extends Component {
+//       constructor() {
+//         super()
+//         this.state = {
+//           employees: [],
+//           pets:[]
+//         }
+//       }
+//       componentDidMount(){
+//           fetch(`https://vet-lab-8-4.herokuapp.com/api/employees`)
+//           .then(data => data.json())
+//           .then(json => {
+//             this.setState({
+//                employees: json
+//            })
+//           })
+//           .catch(error => (error))
 
+//           fetch(`https://vet-lab-8-4.herokuapp.com/api/pets`)
+//           .then(animalsData => animalsData.json())
+//           .then(json =>{
+//             this.setState({
+//               pets: json
+//             })
+//           })
+//         }
+// //Filter our Pets array returning only the animals that have seeing a specific provider based on their Id
+// // Returning and array of pets
+//       render(){
 
-// export const EmployeeList = () => {
-  //   return (
-    //     <main>
-    //       <h2>All Staff</h2>
-    //       <section className="employee-list">
-    
-    //         <Employee />
-    //       </section>
-    //     </main>
-    //   );
-    // };
-    
-    // export default EmployeeList;
-    
-    export default class EmployeeList extends Component {
-      constructor() {
-        super()
-        this.state = {
-          employees: [],
-          pets:[]
-        }
-      }
-      componentDidMount(){
-          fetch(`https://vet-lab-8-4.herokuapp.com/api/employees`)
-          .then(data => data.json())
-          .then(json => {
-            this.setState({
-               employees: json
-           })
-          })
-          .catch(error => (error))
+//         return(
+//           <main>
+//              <h2>All Staff</h2>
+//              <section className="employee-list">
+//               {this.state.employees.map((employee)=>{
+//                 return(<Employee employee ={employee} key ={employee.id} pets = {this.state.pets}/>
+//                 )
+//               })
+//             }
+//              </section>
+//           </main>
+//         )
+//       }
+//     }
 
-          fetch(`https://vet-lab-8-4.herokuapp.com/api/pets`)
-          .then(animalsData => animalsData.json())
-          .then(json =>{
-            this.setState({
-              pets: json
-            })
-          })
-        }
-//Filter our Pets array returning only the animals that have seeing a specific provider based on their Id
-// Returning and array of pets
-      render(){
-      
-        return(
-          <main>
-             <h2>All Staff</h2>
-             <section className="employee-list">
-              {this.state.employees.map((employee)=>{
-                return(<Employee employee ={employee} key ={employee.id} pets = {this.state.pets}/>
-                )
-              })
-            }
-             </section>
-          </main>
-        )
-      }
-    }
+//Class Component replace with useState 
+const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+  const [pets, setPets] = useState([]);
+  fetch(`https://vet-lab-8-4.herokuapp.com/api/employees`)
+    .then((data) => data.json())
+    .then((json) => {
+      setEmployees(json);
+    })
+    .catch((error) => error);
+
+  fetch(`https://vet-lab-8-4.herokuapp.com/api/pets`)
+    .then((animalsData) => animalsData.json())
+    .then((json) => {
+      setPets(json);
+    })
+    .catch((error) => error);
+
+  return (
+    <main>
+      <h2>All Staff</h2>
+      <section className="employee-list">
+        {employees.map((employee) => {
+          return (
+            <Employee
+              employee={employee}
+              key={employee.id}
+              pets={pets}
+            />
+          );
+        })}
+      </section>
+    </main>
+  );
+};
+export default EmployeeList;
