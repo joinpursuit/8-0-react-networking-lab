38b439a3088cef984542a4a4011431f2bb38b36c
KamNotCam <Kedari.a.matthews@gmail.com>
2022-06-20 07:26:51 -0400

    Finalizing some work

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -2,15 +2,33 @@ import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export const EmployeeList = () => {
-  return (
-    <main>
-      <h2>All Staff</h2>
-      <section className="employee-list">
-        <Employee />
-      </section>
-    </main>
-  );
-};
+class EmployeeList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      listemployees: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://serene-tundra-77911.herokuapp.com/api/employees")
+      .then((response) => response.json())
+      .then((obj) => {
+        this.setState({ listemployees: [...obj] });
+      })
+      .catch((error) => console.log(`Error :`, error));
+  }
+  render() {
+    return (
+      <main>
+        <h2>All Staff</h2>
+        <section className="employee-list">
+          {this.state.listemployees.map((e, i) => (
+            <Employee key={i} employee={e} />
+          ))}
+        </section>
+      </main>
+    );
+  }
+}
 
-export default EmployeeList;
+export default EmployeeList
