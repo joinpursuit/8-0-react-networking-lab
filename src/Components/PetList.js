a13be8d120fbad22db69fde13eefdd14962341dd
Soma Mahajaya <sokairavi@gmail.com>
2022-06-26 14:30:07 -0400

    Final React Network

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,35 @@
-export const PetList = () => {
-  return (
+import React from "react";
+
+class PetList extends React.Component{
+
+  constructor() {
+    super();
+    this.state = {
+      pets: [],
+      displayPets: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://vet-api-8-1.herokuapp.com/api/pets?employeeId=' + this.props.employeeID).then((response) => {
+      return response.json()
+  }).then((json) => {
+      this.setState({
+      pets: json,
+      });
+      })
+  }  
+
+  render (){
+    let {pets} = this.state;
+    let {displayPets} = this.props;
+    let pet = pets.length ? pets.map((pet) => pet.name).join(",")
+    : 'No pets listed for this employee.';
+    return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+    <p>{displayPets ? pet : null}</p>
     </aside>
   );
-};
-
+}
+}
 export default PetList;
