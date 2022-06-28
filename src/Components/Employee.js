15f3d656ede64ee1fead0ec9ddbee39bdcdb5fa2
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-06-27 21:26:58 -0400

    add _redirects

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -4,19 +4,18 @@ import "./Employee.css";
 
 const Employee = ({ employee }) => {
   const [showPets, setShowPets] = useState(false);
-  const { id, firstName: FName, lastName : LName, prefix, postfix, title } = employee;
-    return (
-      <article id={id} className="employee">
-        <h3> {postfix ? `${prefix} ${FName} ${LName}, ${postfix}` :
-         `${prefix} ${FName}  ${LName}`}
-        </h3>
-        <h4> {!title ? "Staff Member Title" : `${title}`}</h4>
-        <button 
+  const { id, firstName: FName, lastName: LName, prefix, postfix, title } = employee;
+  return (
+    <article id={id} className="employee">
+      <h3> {postfix ? `${prefix} ${FName} ${LName}, ${postfix}` :
+        `${prefix} ${FName}  ${LName}`}
+      </h3>
+      <h4> {!title ? "Staff Member Title" : `${title}`}</h4>
+      <button
         onClick={() => setShowPets(!showPets)}>{!showPets ? 'Show Pets' : 'Hide Pets'}</button>
-        <PetList id={id} showPets={showPets}/>
-      </article>
-    )
-
+      <PetList id={id} showPets={showPets} />
+    </article>
+  )
 }
 
 export default Employee;
