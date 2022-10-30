ad825d14836cec8d55bc9e96d2a8fda1548a0fd2
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-10-30 18:07:30 -0400

    huh

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,53 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import React, { useState, useEffect } from "react";
 
 export const Employee = () => {
-  return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
-  );
+  let empList = [];
+  const [currEmployees, setEmployees] = useState([]);
+  const [cid, setID] = useState([]);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    fetch("https://vet-resource-api-9-2.herokuapp.com/api/employees", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data[0]);
+        setEmployees(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  empList = currEmployees.map((employee) => {
+    const { firstName, lastName, prefix, postfix, title, id } = employee;
+    let name = `${prefix || ""} ${firstName} ${lastName}${
+      postfix ? ", " + postfix : ""
+    }`;
+    console.log(name);
+    return (
+      <article className="employee">
+        <h3>{name}</h3>
+        <h4>{title || ""}</h4>
+        <button
+          onClick={() => {
+            setToggle(!toggle);
+            setID(id);
+          }}
+        >
+          Show Pets
+        </button>
+        <PetList id={id} toggle={toggle} cid={cid} />
+      </article>
+    );
+  });
+
+  console.log(Array.isArray(currEmployees));
+
+  return <>{empList}</>;
 };
 
 export default Employee;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,40 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
-  );
+import React, { useState, useEffect } from "react";
+
+export const PetList = ({ id, toggle, cid }) => {
+  let empList;
+  const [currPets, setPets] = useState([]);
+
+  useEffect(() => {
+    fetch("https://vet-resource-api-9-2.herokuapp.com/api/pets", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data[0]);
+        setPets(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+  function matchPet(id, currPets, toggle) {
+    console.log(id);
+    let list = currPets.filter((pet) => id === pet.employeeId);
+    if (toggle === false && cid !== id) {
+      return null;
+    } else if (list.length > 0) {
+      return (
+        <ul>
+          {list.map((pet) => (
+            <li>{pet.name}</li>
+          ))}
+        </ul>
+      );
+    } else if (list.length === 0) {
+      return <p>No pets listed for this employee.</p>;
+    }
+  }
+  return <aside className="pets-list">{matchPet(id, currPets, toggle)}</aside>;
 };
 
 export default PetList;
