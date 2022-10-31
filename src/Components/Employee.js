d245d2cd3cc655aa503d706368beadd823633842
Salinamalek <salinamalek@pursuit.org>
2022-10-31 18:51:07 -0400

    lab completed

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,13 +1,21 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState } from "react";
 
-export const Employee = () => {
+export const Employee = ({ employee, theirPets }) => {
+  const [showPets, setShowPets] = useState(false);
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>
+        {employee.prefix + " " + employee.firstName + " " + employee.lastName}
+        {employee.postfix && ", " + employee.postfix}
+      </h3>
+      <h4>{employee.title}</h4>
+      <button onClick={() => setShowPets(!showPets)}>
+        {showPets ? "Hide Pets" : "Show Pets"}
+      </button>
+      {showPets && <PetList employee={employee} theirPets={theirPets} />}
     </article>
   );
 };
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,30 @@
-export const PetList = () => {
+import { useEffect, useState } from "react";
+
+export const PetList = ({ employee }) => {
+  const [theirPets, setTheirPets] = useState([]);
+  function fetchPet() {
+    fetch(`https://vet-api-1.onrender.com/api/pets?employeeId=${employee.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setTheirPets(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  useEffect(() => {
+    fetchPet();
+  }, [employee.id]);
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {theirPets.length === 0 ? (
+        <p>No pets listed for this employee.</p>
+      ) : (
+        theirPets.map((pet) => {
+          return pet.employeeId === employee.id ? <p>{pet.name}</p> : null;
+        })
+      )}
     </aside>
   );
 };
