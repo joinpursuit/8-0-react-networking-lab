fd56069210b3eb6aa25d8a522e7b2fd8d38a5ed0
sreeves07 <shamarreeves@pursuit.org>
2022-10-31 15:52:10 -0400

    full name and title printing

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,14 +1,43 @@
 import PetList from "./PetList";
+import { useState, useEffect } from "react";
 import "./Employee.css";
 
 export const Employee = () => {
+
+  const [employeeInfo, setEmployeeInfo] = useState({})
+
+  function getEmployees () {
+    fetch("https://vet-api-1.onrender.com/api/employees")
+    .then((response) => response.json())
+    .then((json) => {
+      setEmployeeInfo(json);
+    })
+    .catch((err) => {
+      console.log("error fetching image");
+    });
+  }
+
+  useEffect(() => {
+    getEmployees()
+  }, [])
+
+  console.log(employeeInfo)
+
+
   return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+    employeeInfo.map((data) => {
+      return (
+
+        <article className="employee">
+        <h3>{data.prefix}{data.firstName} {data.lastName} {data.postfix}</h3>
+        <h4>{data.title}</h4>
+        <button>Show Pets</button>
+        <PetList />
     </article>
+
+      )
+    })
+    
   );
 };
 
