8c50571f3b238623aff5b537ff2fa52e08eb8199
minagrullon <minagrullon@pursuit.org>
2022-10-31 15:30:18 -0400

    mostly done

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,13 +1,40 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState } from "react";
 
-export const Employee = () => {
+export const Employee = ({ worker, index }) => {
+  // const [employees, setEmployees] = useState([]);
+  const [showPets, setShowPets] = useState(false);
+  // function getEmployees() {
+  //   fetch("https://vet-api-1.onrender.com/api/employees")
+  //     .then((data) => data.json())
+  //     .then((data) => setEmployees(data))
+  //     .catch((err) => {
+  //       console.log("ERR");
+  //     });
+  // }
+
+  function togglePets() {
+    setShowPets(!showPets);
+  }
+
+  // useEffect(() => {
+  //   getEmployees();
+  // }, []);
+
+  // console.log(employees);
+
   return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+    <article className="employee" key={index}>
+      <h3>
+        {worker.prefix} {worker.firstName} {worker.lastName}
+        {!worker.postfix ? null : ", " + worker.postfix}
+      </h3>
+      <h4>{worker.title}</h4>
+      <button onClick={togglePets}>
+        {showPets ? "Hide Pets" : "Show Pets"}
+      </button>
+      {showPets ? <PetList worker={worker} showPets={showPets} /> : null}
     </article>
   );
 };
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,8 +1,32 @@
-export const PetList = () => {
+import { useState, useEffect } from "react";
+
+export const PetList = ({ worker, showPets }) => {
+  const [pets, setPets] = useState([]);
+  function getPets() {
+    fetch(`https://vet-api-1.onrender.com/api/pets?employeeId=${worker.id}`)
+      .then((data) => data.json())
+      .then((data) => setPets(data))
+      .catch((err) => {
+        console.log("ERR");
+      });
+  }
+
+  useEffect(() => {
+    getPets();
+  }, [showPets]);
+
   return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
+    <>
+      {pets
+        ? pets.map((pet, index) => {
+            return (
+              <aside className="pets-list" key={index}>
+                <p>{pet ? pet.name : "No pets listed for this employee."}</p>
+              </aside>
+            );
+          })
+        : null}
+    </>
   );
 };
 
