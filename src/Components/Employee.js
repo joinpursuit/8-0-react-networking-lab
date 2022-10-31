f8f7b36dc69d74b0ea9cec56e737f2e461c52240
TonyH98 <tonyhoang@pursuit.org>
2022-10-31 14:16:48 -0400

    Most functionality added

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,14 +1,35 @@
 import PetList from "./PetList";
+import { useState, useEffect } from "react";
 import "./Employee.css";
 
 export const Employee = () => {
+  const [employee, setEmployee] = useState([])
+ 
+  useEffect(() => {
+    fetch("https://vet-api-1.onrender.com/api/employees")
+    .then((response) => response.json())
+    .then((json) => {
+      setEmployee(json);
+    })
+    .catch((err) => {
+      console.log("error");
+    });
+  }, [])
+
+
   return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
+   <section>
+     {employee.map((map) => {
+       return (
+        <article className="employee">
+        <h3>{map.prefix} {map.firstName} {map.lastName} {map.postfix}</h3>
+        <h4>{map.title}</h4>
+        <PetList map={map}/>
+      </article>
+       )
+     })}
+   </section>
+    
   );
 };
 
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,38 @@
-export const PetList = () => {
+import {useState, useEffect} from "react"
+
+export const PetList = ({map}) => {
+
+  const [pet, setPet] = useState([])
+  const [showInfo, setShowInfo] = useState(false)
+
+  function toggleInfo(){
+    setShowInfo(!showInfo)
+}
+
+useEffect(() => {
+  fetch("https://vet-api-1.onrender.com/api/pets")
+  .then((response) => response.json())
+  .then((json) => {
+    setPet(json);
+  })
+  .catch((err) => {
+    console.log("error");
+  });
+}, [])
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      <button onClick={toggleInfo}>{!showInfo ? "Show Pets": "Don't Show Pets"}</button>
+      {showInfo ? (
+                <p>{pet.map((pets) => {
+                  if(pets.employeeId === map.id){
+                    return(
+                      pets.name
+                    )
+                  }
+                })}</p>
+
+            ): null}
     </aside>
   );
 };
