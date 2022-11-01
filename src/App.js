ed2902e1cc04763af4c0c446eeeb2e055e615930
JamalPartridge <jamalpartridge@pursuit.org>
2022-11-01 02:30:37 -0400

    react networking lab Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
 
 function App () {
+  const [employees, setEmployees] = useState([])
+
+  useEffect(() => {
+    fetch(`https://vet-api-1.onrender.com/api/empolyees`)
+    .then((res) => res.json())
+    .then((res) => {
+      setEmployees(res)
+    })
+    .catch((err) => console.log(err))
+  }, []) 
+
   return (
     <>
       <NavBar />
-      <EmployeeList />
+      <EmployeeList employees={employees} />
     </>
   );
 };
