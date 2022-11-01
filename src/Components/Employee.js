8ae33fe349f7c8caa05d8899618d9a056bd4df89
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-10-31 20:14:47 -0400

    refactored code

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -13,26 +13,19 @@ export const Employee = ({
   const [showPets, setShowPets] = useState(false);
   const [pets, setPets] = useState("");
 
-  //
-
   useEffect(() => {
     fetch("https://vet-api-1.onrender.com/api/pets?employeeId=" + id)
       .then((res) => res.json())
-      .then((res) =>
-        setPets(() => {
-          const petNames = res.map(({ name }) => name);
-          return petNames.join(", ");
-        })
-      )
+      .then((res) => setPets(() => res.map(({ name }) => name).join(", ")))
       .catch((err) => console.log(err));
   }, [id]);
 
   return (
     <article className="employee">
       <h3>
-        {prefix ? prefix + " " : null}
+        {prefix && prefix + " "}
         {firstName + " " + lastName}
-        {postfix ? ", " + postfix : null}
+        {postfix && ", " + postfix}
       </h3>
       <h4>{title}</h4>
       <button onClick={() => setShowPets(!showPets)}>
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,7 @@
 export const PetList = ({ pets, showPets }) => {
   return (
     <aside className="pets-list">
-      <p>{showPets ? pets? <>{pets}</> : "No pets listed for this employee!": null}</p>
+      <p>{showPets && (pets ? pets : "No pets listed for this employee!")}</p>
     </aside>
   );
 };
