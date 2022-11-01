0f42f5e30b69a0271d91a22209df39d60cd65ebb
MarsiaRochelin <marsrochelin@pursuit.org>
2022-10-31 23:01:35 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
 
-function App () {
+function App() {
+  const [employees, setEmployees] = useState([]);
+
+   useEffect(() => {
+    fetch("https://vet-api-1.onrender.com/api/employees")
+    .then((res) => res.json())
+    .then((data) => {
+
+      setEmployees(data);
+    })
+    .catch((err) => {
+      console.log("error fetching employees.");
+    });
+
+   }, [])
+
+console.log(employees)
+
   return (
     <>
       <NavBar />
-      <EmployeeList />
+      <EmployeeList 
+      employees={employees}
+     
+      />
     </>
   );
-};
+}
 
 export default App;
