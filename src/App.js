d43fc8a0778ff3170e9114d2fd1acec1d824cc8b
melanieinpursuit <melaniegonzalez@pursuit.org>
2022-11-01 01:58:29 -0400

    Finished lab.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
 
 function App () {
+  const [employees, setEmployees] = useState("")
+  const [pets, setPets] = useState("")
+
+  function allEmployees() {
+    fetch("https://vet-api-1.onrender.com/api/employees")
+    .then((response) => response.json())
+    .then((json) => {
+     setEmployees(json)
+    })
+    .catch((err) => {
+      console.log("Error fetching data.");
+    });
+  }
+  
+  useEffect(() => {
+    allEmployees()
+  }, [])
+  
   return (
     <>
       <NavBar />
-      <EmployeeList />
+      <EmployeeList employees={employees} key={employees.id} />
     </>
   );
-};
+}
 
 export default App;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,9 +1,31 @@
-export const PetList = () => {
+import { useState, useEffect } from "react";
+
+
+export const PetList = ({eachEmployeeId}) => {
+
+  const [pets, setPet]= useState([])
+
+function petList(){
+  fetch(`https://vet-api-1.onrender.com/api/pets?employeeId=${eachEmployeeId}`)
+  .then((response) => response.json())
+  .then((json) => {
+   setPet(json)
+  })
+  .catch((err) => {
+    console.log("Error fetching data.");
+  });
+}
+
+useEffect(()=>{
+  petList()
+}, [])
+  
+  
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {pets.length === 0 ? <p>No pets listed for this employee.</p> : pets.map((pet)=>{
+         return pet.employeeId === eachEmployeeId ? <p>{pet.name}</p>: null})}
     </aside>
-  );
-};
-
+    )
+  }
 export default PetList;
