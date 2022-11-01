302effbf05d068c1bdd6bdab9bbf769427d4e278
Wwright91 <wisdomwright-martin@pursuit.org>
2022-11-01 10:17:25 -0400

    react-networking-completed

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+  function makeFetchRequest() {
+    fetch("https://vet-resource-api-9-2.herokuapp.com/api/employees")
+      .then((response) => response.json())
+      .then((response) => setEmployees(response));
+  }
+
+  useEffect(() => {
+    makeFetchRequest();
+  }, []);
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map(
+          ({ firstName, lastName, prefix, postfix, title, id }) => {
+            return (
+              <Employee
+                key={id}
+                id={id}
+                firstName={firstName}
+                lastName={lastName}
+                prefix={prefix}
+                postfix={postfix}
+                title={title}
+              />
+            );
+          }
+        )}
       </section>
     </main>
   );
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,31 @@
-export const PetList = () => {
+import { useEffect, useState } from "react";
+
+export const PetList = ({ id }) => {
+  const [pets, setPets] = useState([]);
+
+  function makePetsFetchRequest() {
+    fetch(
+      "https://vet-resource-api-9-2.herokuapp.com/api/pets?employeeId=" + id
+    )
+      .then((response) => response.json())
+      .then((response) => setPets(response));
+  }
+
+  useEffect(() => {
+    makePetsFetchRequest();
+  }, []);
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {pets.length ? (
+        pets.map(({ name, id }) => (
+          <ul>
+            <li key={name + id}>{name}</li>
+          </ul>
+        ))
+      ) : (
+        <p>No pets listed for this employee.</p>
+      )}
     </aside>
   );
 };
