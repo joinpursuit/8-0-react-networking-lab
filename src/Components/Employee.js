e5728c22f4c38f45f22d861bcc256d3864119767
DanielR2105 <rodriguez.daniel2105@gmail.com>
2022-11-01 02:24:16 -0400

    finished lab

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,13 +1,34 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState } from "react";
 
-export const Employee = () => {
+export const Employee = ({employee}) => {
+
+  const [showPets, setShowPets] = useState(false);
+  let fullName = ""
+
+  function properName(employee) {
+    if (employee.prefix) {
+      fullName = `${employee.prefix} ${employee.firstName} ${employee.lastName}`
+    } else if (employee.postfix) {
+      fullName = `${employee.firstName} ${employee.lastName}, ${employee.postfix}`
+    } else {
+      fullName = `${employee.firstName} ${employee.lastName}`
+    }
+    return fullName
+  }
+
+  properName(employee)
+
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{fullName}</h3>
+      <h4>{employee.title}</h4>
+      <button onClick={() => setShowPets(!showPets)}>Show Pets</button>
+      <div id={showPets ? "show" : "hidden"}>
+      <PetList id={employee.id}/>
+      </div>
     </article>
   );
 };
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,34 @@
 import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
+import { useState, useEffect } from "react";
 
 export const EmployeeList = () => {
+
+  const [employees, setEmployees] = useState([])
+
+  function fetchDoctors() {
+    fetch("https://vet-api-1.onrender.com/api/employees")
+    .then((response) => response.json())
+    .then((json) => {
+      setEmployees(json);
+    })
+    .catch((err) => {
+      console.log("error fetching employees");
+    });
+  }
+
+  useEffect(() => {
+    fetchDoctors()
+  }, [])
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+       {employees.map((employee) => {
+        return <Employee employee={employee} key={employee.id}/>
+       })}
       </section>
     </main>
   );
