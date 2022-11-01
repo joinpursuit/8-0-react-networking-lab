a1cbd1bed96c4665dfc5888d4c2bf353f61a077f
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-10-31 20:39:14 -0400

    passed

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,25 @@
-export const PetList = () => {
+import { useState, useEffect } from "react";
+
+export const PetList = ({ fetchData, employee }) => {
+  console.log(employee.id);
+  const PET_ENDPOINT = `/api/pets?employeeId=${employee.id}`;
+
+  // ********* STATES *********/
+  let [petData, setPetData] = useState([]);
+
+  useEffect(() => {
+    fetchData(PET_ENDPOINT, setPetData);
+    console.log(petData);
+  }, []);
+
+  //********* RETURN *********/
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {petData.map((pet) => {
+        return petData !== [] ? <p>{`${pet.name}, `}</p> : null;
+      })}
+
+      <p>No pets listed.</p>
     </aside>
   );
 };
