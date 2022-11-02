13745d8da0c58b6e8025ed2ce0eaf50ebabe5710
Tech-Kat <katpelle@pursuit.org>
2022-11-02 14:40:10 -0400

    done

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,35 @@
-export const PetList = () => {
+import { useEffect, useState } from "react";
+
+export const PetList = ({id}) => {
+
+  const [pet, setPet] = useState([]);
+
+  function fetchRequest() {
+    //fetch api if employeeid = id key (api/pets?employeeId="+id)
+    fetch("https://vet-resource-api-9-2.herokuapp.com/api/pets?employeeId="+id)
+      .then((response) => response.json())
+      .then((petData) => setPet(petData));
+  }
+
+  console.log(pet);
+  useEffect(() => {
+    fetchRequest();
+  }, []);
+
+
+  
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {pet.length ? 
+      pet.map(({id,name}) => {
+        return (
+          <ul>
+            <li key={id}>{name}</li>
+          </ul>
+         )
+      }) :  <p>No pets listed for this employee.</p>
+      }
+     
     </aside>
   );
 };
