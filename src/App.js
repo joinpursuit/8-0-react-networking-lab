359056570e317aac3c204593a3dabc63012d1487
Kylie-Lang1 <kylielang@pursuit.org>
2022-11-04 02:32:50 -0400

    Functioning show pets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
 
 function App () {
+
+  const [employees, setEmployees] = useState([])
+
+  const BASE_URL = "https://vet-api-1.onrender.com"
+  const pets = "/api/pets"
+  const owners =  "/api/owners"
+  const owners_pets = "/api/owners_pets"
+  const employee = "/api/employees"
+
+  function getEmployees() {
+    fetch(BASE_URL + employee)
+      .then((res) => res.json())
+      .then((res) => {
+        setEmployees(res)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+useEffect(() => {
+  getEmployees()
+}, [])
+
   return (
     <>
       <NavBar />
-      <EmployeeList />
+      <EmployeeList employees={employees} />
     </>
-  );
+  ); 
 };
 
 export default App;
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,58 @@
-export const PetList = () => {
+import { useState, useEffect } from "react";
+
+export const PetList = ({ employeeID }) => {
+
+  const [showPets, setShowPets] = useState(false)
+
+  const BASE_URL = "https://vet-api-1.onrender.com/api/pets?employeeId="
+  const [pets, setPets] = useState([])
+
+  useEffect(() => {
+    fetch(BASE_URL + employeeID)
+      .then((res) => res.json())
+      .then((res) => {
+        setPets(res)
+      })
+      .catch((error) => console.log(error))    
+  }, [])
+
+  function handleClick () {
+    setShowPets(!showPets)
+  }
+
+  function showDetails () {
+    pets.map((pet, i) => {
+      if (i === pets.length -1){
+        return `${pet.name}`
+      } else if (i < pets.length -1 ){
+        return `${pet.name}, `
+      } else if (pets === []){
+        return "No pets listed for this employee"
+      }
+    }) 
+  }
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      <button onClick={handleClick}>Show Pets</button>
+      {
+        showPets ? (
+          <p>
+            {
+            pets.map((pet, i) => {
+              if (i === pets.length -1){
+                return `${pet.name}`
+              } else if (i < pets.length -1 ){
+                return `${pet.name}, `
+              } else if (!pet){
+                return "No pets listed for this employee"
+              }
+            }) 
+            }
+          </p>
+        ) :
+        null
+      }
     </aside>
   );
 };
