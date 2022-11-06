aeed897b78a49812f9d1da816640c0fed8e0595f
ZayLpursuit <Zaylors@pursuit.org>
2022-11-06 12:43:13 -0500

    Working

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,33 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useEffect, useState } from "react";
 
-export const Employee = () => {
+
+export const Employee = ({employee}) => {
+
+  const [showPetList,setShowPetList]=useState(false)
+  let fullName= employee.prefix + " "+ employee.firstName + " " + employee.lastName + ", " + employee.postfix
+  let title=employee.title
+
+  // function petList
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{fullName}</h3>
+      <h4>{title}</h4>
+      <button onClick={()=>setShowPetList(true)}>Show Pets</button>
+      { showPetList && <PetList id={employee.id}/>}
     </article>
   );
 };
 
 export default Employee;
+
+/*
+Api request (useeffects)
+convert data and store in variables
+set state for pet data on click
+add states  into dependency array 
+
+
+*/ 
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
+
 export const EmployeeList = () => {
+const [vetEmpl,setVetEmpl]=useState([])
+  useEffect(()=>{
+    fetch("https://vet-resource-api-9-2.herokuapp.com/api/employees")
+    .then((response)=>response.json())
+    .then((response)=>setVetEmpl(response))
+  },[])
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+       { vetEmpl.map((employee)=>{
+        return <Employee employee={employee} />
+       })
+
+}
       </section>
     </main>
   );
