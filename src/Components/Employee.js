8f294e80814c026d515ef2c41e5eb3c4d05a5bb7
“danielsolaquegit config --global user.email “danielsolaque@pursuit.org <example@email.com>
2022-11-06 20:21:25 -0500

    lets do it

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,13 +1,36 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState } from "react";
+import { getPets } from "../api/veterinaryApi";
 
-export const Employee = () => {
+export const Employee = ({ data }) => {
+  const [pets, setPets] = useState([])
+  const [showPets, setShowPets] = useState(false)
+  const { firstName, lastName, title, prefix, postfix } = data
+
+  const generateName = () => {
+    const fullName = `${firstName} ${lastName}`
+
+    if (prefix) return `${prefix} ${fullName}`
+    if (postfix) return `${fullName}, ${postfix}`
+
+    return fullName
+  }
+
+  const handleClick = () => {
+    getPets(data.id)
+      .then(petsList => setPets(petsList))
+
+    setShowPets(true)
+  }
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{generateName()}</h3>
+      <h4>{title}</h4>
+      <button onClick={()=> handleClick()}>Show Pets</button>
+
+      { showPets ? <PetList pets={pets} /> : null }
     </article>
   );
 };
