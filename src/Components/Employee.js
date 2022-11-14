d647ef2a4fb42f66836811882d1d816c19874c7a
bigdaddyjean <jeannappoleon@pursuit.org>
2022-11-14 17:44:49 -0500

    finished

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,13 +1,27 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState } from "react";
+import { getEmployeePet } from "../api/fetch";
 
-export const Employee = () => {
+export const Employee = ({ employee }) => {
+  const [pets, setPets] = useState([]);
+  const [petSwitch, setPetSwitch] = useState(false);
+  const handleToggle = () => {
+    if (pets.length === 0)
+      getEmployeePet(employee.id).then((data) => setPets(data));
+    else setPets();
+    setPetSwitch(!petSwitch);
+  };
+  const postFix = employee.postfix ? `, ${employee.postfix}` : "";
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>
+        {employee.prefix} {employee.firstName} {employee.lastName}
+        {postFix}
+      </h3>
+      <h4>{employee.title}</h4>
+      <button onClick={handleToggle}>{petSwitch ? "Hide" : "Show"} Pets</button>
+      {petSwitch && <PetList pets={pets.map((pet) => pet.name)} />}
     </article>
   );
 };
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
+import { getEmployees} from "../api/fetch"
 
 export const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    getEmployees().then((data) => setEmployees(data));
+  }, []);
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map((employee) => (
+          <Employee
+            key={employee.id}
+            employee={employee}
+          />
+        ))}
       </section>
     </main>
   );
