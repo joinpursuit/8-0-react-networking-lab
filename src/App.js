4250faa1972493e5df6186fa2082a27262ed7307
jasn4real <tobeobi@pursuit.org>
2022-12-10 21:38:02 -0500

    No API calls were made

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
+import { useState, useEffect } from "react";
+
+
+const EMPLOYEE_API = "/employee.json";
+const PETS_API = "/pets.json";
+//Can't fetch API so broke up and saved data, and fetched from storage
 
 function App () {
+
+  const [pets, setPets] = useState([]);
+  const [employees, setEmployees] = useState([]);
+
+  function getEmployees() {
+    fetch(EMPLOYEE_API)
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployees(data);
+      })
+
+      .catch((err) => {
+        console.log("error fetching data");
+      });
+  }
+  useEffect(() => {
+    getEmployees();
+  }, []);
+
+  function getPets() {
+    fetch(PETS_API)
+      .then((response) => response.json())
+      .then((data) => {
+        setPets(data);
+      })
+      .catch((err) => {
+        console.log("error fetching data");
+      });
+  }
+  useEffect(() => {
+    getPets();
+  }, []);
+
   return (
     <>
       <NavBar />
-      <EmployeeList />
+      <EmployeeList employees={employees} pets={pets} />
     </>
   );
 };
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,19 @@
-export const PetList = () => {
+import React from "react";
+
+
+const PetList = ({ employee, pets }) => {
+  
+  const filteredPets = pets.filter((pet) => pet.employeeId === employee.id);
+  const filtereredPetlist = filteredPets.map((thisPet) => thisPet.name);
+
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {filteredPets.length ? (
+        <p>{filtereredPetlist.join(", ")}</p>
+      ) : (
+        <article>No pets listed for this employee.</article>
+      )}
     </aside>
   );
 };
